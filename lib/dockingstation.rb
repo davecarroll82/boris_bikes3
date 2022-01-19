1122dab72211a66a1f51bd1116143cf1e678920d
David <dave_carroll82@hotmail.com>
2022-01-19 16:34:30 +0000

    added bike class and spec

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,7 +1,9 @@
+require_relative 'bike'
+
 class DockingStation
 
   def release_bike
-    "bike released"
+    bike = Bike.new
   end 
 
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,10 +1,14 @@
 require 'dockingstation'
 
 describe DockingStation do
-
-  it 'can release the bike' do
-  
   docking_station = DockingStation.new
-  expect(docking_station.release_bike).to eq("bike released")
+
+  it 'responds to release_bike' do
+    expect(docking_station).to respond_to :release_bike
+  end 
+
+it 'releases working bike' do
+  expect(docking_station.release_bike).to be_working
   end
+
 end
