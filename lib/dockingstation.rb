29bb41fe8881cacb725525a887828fc60d5a0c05
David <dave_carroll82@hotmail.com>
2022-01-21 11:28:13 +0000

    Co-authored-by: Barrington Brook <Barrington-Brook@users.noreply.github.com>

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,13 +1,24 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
 
   def release_bike
-    bike = Bike.new
+    if @bikes.empty?
+      raise "no bikes available"
+    else
+      @bikes.pop
+    end
   end 
 
   def dock(bike)
-
+    @bikes.push(bike)
   end
+
 end
+
 
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -7,13 +7,20 @@ describe DockingStation do
     expect(docking_station).to respond_to :release_bike
   end 
 
-it 'releases working bike' do
-  expect(docking_station.release_bike).to be_working
+  it 'releases working bike' do
+    expect(docking_station.release_bike).to be_working
   end
 
   it 'respond to dock with bike' do
     bike = Bike.new
-  expect(docking_station.dock(bike)).to eq(bike)
+    docking_station.dock(bike)
+    expect(docking_station.bikes).to include(bike)
   end
 
+  it "raise an error if we ask for a bike and there are none" do
+    docking_station = DockingStation.new
+    expect {docking_station.release_bike}.to raise_error("no bikes available")
+  end
 end
+
+
